3e64c66b981145077d99757b9a9d85e14cda2614
LizGb68 <dgonzalezb9@miumg.edu.gt>
2025-04-04 01:32:27 -0600

    modificacion de menu principal por Doris

--- menu_principal.cpp
+++ menu_principal.cpp
@@ -4,6 +4,11 @@
 #include "conversiones.h"
 #include "palindromo.h"
 #include "contar_palabras.h"
+#include "ArabigosARomanos.h"
+#include "EnterosALetras.h"
+#include "DecimalesALetras.h"
+#include "TablaMultiplicar.h"
+#include "Temporizador.h"
 #include "tabla_multiplicar.h"
 #include "multiplicacion_manual.h"
 #include "decimal_binario.h"
@@ -67,6 +72,70 @@ void menuHugo() {
     } while (opcionH != 6);  
 }
 
+void menuDoris(){
+	cout << "\n===== Menu de programas realizados por Doris Gonzalez =====" << endl;
+	cout << "1. Numeros Arabigos a Romanos" <<endl;
+	cout << "2. Numeros Enteros a Letras" <<endl;
+	cout << "3. Numeros Enteros con Decimales  a Letras" <<endl;
+	cout << "4. Tabla de Multiplicar" <<endl;
+	cout << "5. Temporizador" <<endl;
+	cout << "Ingrese una opcion: ";
+
+    int opcionD;
+
+    do {
+        menuDoris(); 
+        cin >> opcionD;  
+
+        switch (opcionD) {
+            case 1:
+                convertirNumeroRomano();
+                break;
+
+            case 2:
+                int num;
+			    cout << "Introduce un numero entero (0-1000): ";
+			    cin >> num;
+			    convertirNumero(num);
+			    cout << endl;
+                break;
+
+            case 3:
+                float nums;
+			    cout << "Introduce un numero con decimales: ";
+			    cin >> nums;
+			    cout << nums << "\n";
+			    convertirNumero(nums);
+			    cout << endl;
+                break;
+
+            case 4: {
+                tablaMultiplicar();
+                break;
+            }
+
+            case 5:
+            	int horas, minutos, segundos;
+
+			    cout << "Ingrese las horas: ";
+			    cin >> horas;
+			    cout << "Ingrese los minutos: ";
+			    cin >> minutos;
+			    cout << "Ingrese los segundos: ";
+			    cin >> segundos;
+                temporizador(horas, minutos, segundos);
+                break;
+
+            case 6:
+                cout << "Saliendo. . ." << endl;
+                break;
+
+            default:
+                cout << "Opción no encontrada. Intente de nuevo." << endl;
+        }
+
+    } while (opcionD != 6);  
+}
 
 void menuLuis() {
     cout << "\n===== Menu de programas realizados por Luis Martínez =====" << endl;
